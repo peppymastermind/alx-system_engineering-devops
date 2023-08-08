ef0ccfcf25eeba18ded673db79718c4f5ee07802
peppymastermind <koranchieperpetual94@gmail.com>
2023-08-08 15:50:55 +0000

    webstack debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Puppet manifest to correct the file extension typo in wp-settings.php
+
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
